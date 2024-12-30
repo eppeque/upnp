08371c4aab7a808eb191c78ce8e019f260c8f080
Quentin EPPE <quentin.eppe@nrb.be>
2024-12-30 15:49:17 +0100

    Init commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+/**
+ * Returns the sum of two numbers
+ * @param x The first number
+ * @param y The second number
+ * @returns The sum of both numbers
+ */
+export function add(x: number, y: number): number {
+  return x + y;
+}
