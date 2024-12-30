f3e91772264c366590e75596ada24791ab99128d
Quentin Eppe <eppe.quentin@gmail.com>
2024-12-30 22:58:41 +0100

    Exported members

--- index.ts
+++ index.ts
@@ -1,9 +1,4 @@
-/**
- * Returns the sum of two numbers
- * @param x The first number
- * @param y The second number
- * @returns The sum of both numbers
- */
-export function add(x: number, y: number): number {
-  return x + y;
-}
+import { SharepointService } from "./src/sharepoint";
+import type { BaseItem } from "./src/base_item";
+
+export { SharepointService, type BaseItem };
