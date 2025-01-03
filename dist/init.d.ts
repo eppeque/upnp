d35b31853954d13b03659247c5e89996f56429c2
Quentin EPPE <quentin.eppe@nrb.be>
2025-01-03 09:13:03 +0100

    Added doc comments to initSharepoint function in built files

--- dist/init.d.ts
+++ dist/init.d.ts
@@ -1,2 +1,7 @@
 import { ISPFXContext, SPFI } from "@pnp/sp";
+/**
+ * Initializes the Sharepoint factory interface based on the Webpart context.
+ * @param context The context of the Sharepoint Webpart.
+ * @returns The Sharepoint factory interface.
+ */
 export declare function initSharepoint(context: ISPFXContext): SPFI;
--- dist/init.js
+++ dist/init.js
@@ -1,4 +1,9 @@
 import { spfi, SPFx } from "@pnp/sp";
+/**
+ * Initializes the Sharepoint factory interface based on the Webpart context.
+ * @param context The context of the Sharepoint Webpart.
+ * @returns The Sharepoint factory interface.
+ */
 export function initSharepoint(context) {
     return spfi().using(SPFx(context));
 }
